34ea306a4389252dd4cba02bfb1d5d68c859e1c7
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2022-10-01 04:54:18 -0700

    Refactor the order of functions

--- js/script.js
+++ js/script.js
@@ -14,48 +14,6 @@ For assistance:
 
 
 
-/** Displays the students of a paticular page.
- * @param {array} list - An array of student objects.
- * @param {number} page - The pagination button page number.
- */
-function showPage(list, page) {
-  const startIndex = page * 9 - 9;
-  let endIndex = getEndingIndex(page, list);
-
-  displayStudents(startIndex, endIndex, list);
-}
-
-function getEndingIndex(page, listOfStudents) {
-  let endIndex = page * 9;
-
-  if (endIndex > listOfStudents.length) {
-    const diff = endIndex - listOfStudents.length;
-    endIndex -= diff;
-  }
-  return endIndex;
-}
-
-function displayStudents(startIndex, endIndex, list) {
-  const studentList = document.querySelector('.student-list');
-
-  studentList.innerHTML = '';
-
-  for (let i = startIndex; i >= startIndex && i < endIndex; i++) {
-    const student = `
-    <li class="student-item cf">
-      <div class="student-details">
-        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
-        <h3>${list[i].name.first} ${list[i].name.last}</h3>
-        <span class="email">${list[i].email}</span>
-      </div>
-      <div class="joined-details">
-        <span class="date">Joined ${list[i].registered.date}</span>
-      </div>
-    </li>`;
-    studentList.insertAdjacentHTML('beforeend', student);
-  }
-}
-
 /** Creates and displays pagination buttons
  * @param {array} list - An array of student objects.
  */
@@ -114,6 +72,48 @@ function removeActiveClassFromPreviousButton() {
 
 function addActiveClassToCurrentButton(event) {
   event.target.classList.add('active');
+}
+
+/** Displays the students of a paticular page.
+ * @param {array} list - An array of student objects.
+ * @param {number} page - The pagination button page number.
+ */
+ function showPage(list, page) {
+  const startIndex = page * 9 - 9;
+  let endIndex = getEndingIndex(page, list);
+
+  displayStudents(startIndex, endIndex, list);
+}
+
+function getEndingIndex(page, listOfStudents) {
+  let endIndex = page * 9;
+
+  if (endIndex > listOfStudents.length) {
+    const diff = endIndex - listOfStudents.length;
+    endIndex -= diff;
+  }
+  return endIndex;
+}
+
+function displayStudents(startIndex, endIndex, list) {
+  const studentList = document.querySelector('.student-list');
+
+  studentList.innerHTML = '';
+
+  for (let i = startIndex; i >= startIndex && i < endIndex; i++) {
+    const student = `
+    <li class="student-item cf">
+      <div class="student-details">
+        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+        <h3>${list[i].name.first} ${list[i].name.last}</h3>
+        <span class="email">${list[i].email}</span>
+      </div>
+      <div class="joined-details">
+        <span class="date">Joined ${list[i].registered.date}</span>
+      </div>
+    </li>`;
+    studentList.insertAdjacentHTML('beforeend', student);
+  }
 }
 
 // Call functions
